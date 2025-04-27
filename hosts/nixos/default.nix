f627712dbd2925a42a3bf0536dbe7c1d9e8c255c
vitune <suzdalev.vic@gmail.com>
2025-04-27 18:36:32 +0000

    final config

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -63,19 +63,6 @@ let user = "nixos";
     };
   };
 
-  # Don't require password for users in `wheel` group for these commands
-  security.sudo = {
-    enable = true;
-    extraRules = [{
-      commands = [
-       {
-         command = "${pkgs.systemd}/bin/reboot";
-         options = [ "NOPASSWD" ];
-        }
-      ];
-      groups = [ "wheel" ];
-    }];
-  };
-
   system.stateVersion = "24.11"; # Don't change this
+
 }
--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -15,24 +15,4 @@ in
 
   programs = shared-programs;
 
-  # This installs my GPG signing keys for Github
-  # systemd.user.services.gpg-import-keys = {
-  #   Unit = {
-  #     Description = "Import gpg keys";
-  #     After = [ "gpg-agent.socket" ];
-  #   };
-
-  #   Service = {
-  #     Type = "oneshot";
-  #     ExecStart = toString (pkgs.writeScript "gpg-import-keys" ''
-  #       #! ${pkgs.runtimeShell} -el
-  #       ${lib.optionalString (gpgKeys!= []) ''
-  #       ${pkgs.gnupg}/bin/gpg --import ${lib.concatStringsSep " " gpgKeys}
-  #       ''}
-  #     '');
-  #   };
-
-  #   Install = { WantedBy = [ "default.target" ]; };
-  # };
-
 }
