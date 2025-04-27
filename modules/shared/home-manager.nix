5c3a2a4e3a39d98f651cd831e8eafb99fcc5e836
victor.suzdalev <victor.suzdalev@aliexpress.ru>
2025-04-27 14:34:13 +0300

    new stable config

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -14,6 +14,18 @@ let user = "victor.suzdalev"; in
     };
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    plugins = [
+      {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./config;
+          file = "p10k.zsh";
+      }
+    ];
   };
 
   direnv = {
