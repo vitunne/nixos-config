a2cd4964ad86971fc72e62e5a342e4050fb85fae
vitune <suzdalev.vic@gmail.com>
2025-04-27 18:26:58 +0000

    config

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -8,8 +8,31 @@ in
   home = {
     enableNixpkgsReleaseCheck = false;
     username = "${user}";
-    homeDirectory = lib.mkDefault "/home/${user}";
+    homeDirectory = "/home/${user}";
     packages = pkgs.callPackage ./packages.nix {};
     stateVersion = "24.11";
   };
+
+  programs = shared-programs;
+
+  # This installs my GPG signing keys for Github
+  # systemd.user.services.gpg-import-keys = {
+  #   Unit = {
+  #     Description = "Import gpg keys";
+  #     After = [ "gpg-agent.socket" ];
+  #   };
+
+  #   Service = {
+  #     Type = "oneshot";
+  #     ExecStart = toString (pkgs.writeScript "gpg-import-keys" ''
+  #       #! ${pkgs.runtimeShell} -el
+  #       ${lib.optionalString (gpgKeys!= []) ''
+  #       ${pkgs.gnupg}/bin/gpg --import ${lib.concatStringsSep " " gpgKeys}
+  #       ''}
+  #     '');
+  #   };
+
+  #   Install = { WantedBy = [ "default.target" ]; };
+  # };
+
 }
